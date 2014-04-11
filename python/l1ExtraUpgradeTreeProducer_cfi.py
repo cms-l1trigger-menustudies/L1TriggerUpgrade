f9486f3df48f0059c39e6296352fca9f3daaf849
Georgia <georgia.karapostoli@gmail.com>
2014-04-11 17:17:44 +0200

    updated tkEle/tkPhoton producers and labels

--- python/l1ExtraUpgradeTreeProducer_cfi.py
+++ python/l1ExtraUpgradeTreeProducer_cfi.py
@@ -5,27 +5,30 @@ l1ExtraUpgradeTreeProducer = cms.EDAnalyzer("L1ExtraUpgradeTreeProducer",
    isoEGLabel = cms.untracked.InputTag("SLHCL1ExtraParticlesNewClustering","IsoEGamma"),
 
    tkEGLabel = cms.untracked.InputTag("L1TkElectronsNewclus","EG"),
+   tkEG2Label = cms.untracked.InputTag("L1TkElectronsNewclusTrkPt7","EG"),
    tkIsoEGLabel = cms.untracked.InputTag("L1TkIsoElectronsNewclus","EG"),
-   tkEMLabel = cms.untracked.InputTag("L1TkPhotons","EGIsoTrk"),
+   tkEMLabel = cms.untracked.InputTag("L1TkPhotonsNewclus","IsoTrk"),
     
    tauLabel = cms.untracked.InputTag("SLHCL1ExtraParticles:Taus"),
    isoTauLabel = cms.untracked.InputTag("SLHCL1ExtraParticles:IsoTaus"),
    tkTauLabel = cms.untracked.InputTag("L1TkTauFromCalo",""),                                            
 
-   jetLabel = cms.untracked.InputTag("L1TowerJetPUSubtractedProducer:PUSubCen8x8"),
-
+#   jetLabel = cms.untracked.InputTag("L1TowerJetPUSubtractedProducer:PUSubCen8x8"),
+   jetLabel = cms.untracked.InputTag("L1CalibFilterTowerJetProducer","CalibratedTowerJets"),   # Calib L1jets                                            
    tkMuonLabel = cms.untracked.InputTag("L1TkMuons",""),                                            
-#  tkJetLabel = cms.untracked.InputTag("L1TkJets","Central"),                                            
-   tkJetLabel = cms.untracked.InputTag("L1TkJetsHI","Central"),  ## Use the HI HLT version of L1Jets                                          
-   
+   tkJetLabel = cms.untracked.InputTag("L1TkJets","Central"),                                            
+#   tkJetLabel = cms.untracked.InputTag("L1TkJetsHI","Central"),  ## Use the HI HLT version of L1Jets                                          
+
    fwdJetLabel = cms.untracked.InputTag("l1extraParticles","Forward"),
    #L1CalibFilterTowerJetProducer:Fwd8x8"),
    muonLabel = cms.untracked.InputTag("l1UpgradeMuonIsolator"),
-   metLabel = cms.untracked.InputTag("rawSLHCL1ExtraParticles:MET"),
+   metLabel = cms.untracked.InputTag("L1EnergySumProducer","MET"),
+   #metLabel = cms.untracked.InputTag("rawSLHCL1ExtraParticles:MET"),
    tkMetLabel = cms.untracked.InputTag("L1TkEtMiss","MET"),
-                                            
-   mhtLabel = cms.untracked.InputTag("L1CalibFilterTowerJetProducer:TowerMHT"),
-   tkMhtLabel = cms.untracked.InputTag("L1TkHTMissCaloHI",""),                                            
 
+   mhtLabel = cms.untracked.InputTag("L1CalibFilterTowerJetProducer:TowerMHT"),
+   tkMhtLabel = cms.untracked.InputTag("L1TkHTMissVtx",""),
+#   tkMhtLabel = cms.untracked.InputTag("L1TkHTMissCalo",""),                                            
+# tkMhtLabel = cms.untracked.InputTag("L1TkHTMissCaloHI",""), # HI version                                            
    maxL1Extra = cms.uint32(20)
 )
--- python/l1Stage2NtupleFromRAW.py
+++ python/l1Stage2NtupleFromRAW.py
@@ -62,41 +62,7 @@ process.p += process.L1TkEtMiss
 process.load("SLHCUpgradeSimulations.L1TrackTriggerObjects.L1TkElectronTrackProducer_cfi")
 
 # "Tkphotons" :
-process.L1TkPhotons = cms.EDProducer("L1TkEmParticleProducer",
-                                             label = cms.string("EGIsoTrk"), # labels the collection of L1TkEmParticleProducer that is produced.
-                                                                                     # e.g. EG or IsoEG if all objects are kept, or
-                                                                                     # EGIsoTrk or IsoEGIsoTrk if only the EG or IsoEG
-                                                                                     # objects that pass a cut RelIso < RelIsoCut are written
-                                                                                     # into the new collection.
-                                             L1EGammaInputTag = cms.InputTag("SLHCL1ExtraParticlesNewClustering","EGamma"),
-    #"SLHCL1ExtraParticles","EGamma"),      # input L1EG collection
-                                                                                     # When the standard sequences are used :
-                                                                                     #   - for the Run-1 algo, use ("l1extraParticles","NonIsolated")
-                                                                                     #     or ("l1extraParticles","Isolated")
-                                                                                     #   - for the "old stage-2" algo (2x2 clustering), use
-                                                                                     #     ("SLHCL1ExtraParticles","EGamma") or ("SLHCL1ExtraParticles","IsoEGamma")
-                                                                                     #   - for the new clustering algorithm of Jean-Baptiste et al,
-                                                                                     #     use ("SLHCL1ExtraParticlesNewClustering","IsoEGamma") or
-                                                                                     #     ("SLHCL1ExtraParticlesNewClustering","EGamma").
-                                             ETmin = cms.double( -1 ),               # Only the L1EG objects that have ET > ETmin in GeV
-                                                                                     # are considered. ETmin < 0 means that no cut is applied.
-                                             RelativeIsolation = cms.bool( True ),   # default = True. The isolation variable is relative if True,
-                                                                                     # else absolute.
-                                             IsoCut = cms.double( 0.1 ),             # Cut on the (Trk-based) isolation: only the L1TkEmParticle for which
-                                                                                     # the isolation is below RelIsoCut are written into
-                                                                                     # the output collection. When RelIsoCut < 0, no cut is applied.
-                                                                                     # When RelativeIsolation = False, IsoCut is in GeV.
-                                                # Determination of the isolation w.r.t. L1Tracks :
-                                             L1TrackInputTag = cms.InputTag("L1Tracks","Level1TkTracks"),
-                                             ZMAX = cms.double( 25. ),       # in cm
-                                             CHI2MAX = cms.double( 100. ),
-                                             PTMINTRA = cms.double( 2. ),    # in GeV
-                                             DRmin = cms.double( 0.07),
-                                             DRmax = cms.double( 0.30 ),
-                                             PrimaryVtxConstrain = cms.bool( False ),  # default = False
-                                             DeltaZMax = cms.double( 999. ),    # in cm. Used only when PrimaryVtxConstrain = True
-                                             L1VertexInputTag = cms.InputTag("NotUsed"),     # Used only when PrimaryVtxConstrain = True
-                                     )
+process.load("SLHCUpgradeSimulations.L1TrackTriggerObjects.L1TkEmParticleProducer_cfi")
 
 process.p += process.L1TkPhotons
 
@@ -112,9 +78,19 @@ process.L1TkElectronsNewclus = process.L1TkElectrons.clone()
 process.L1TkElectronsNewclus.L1EGammaInputTag = cms.InputTag("SLHCL1ExtraParticlesNewClustering","EGamma")
 process.p += process.L1TkElectronsNewclus
 
+process.L1TkElectronsNewclusTrkPt7 = process.L1TkElectrons.clone()
+process.L1TkElectronsNewclusTrkPt7.L1EGammaInputTag = cms.InputTag("SLHCL1ExtraParticlesNewClustering","EGamma")
+process.L1TkElectronsNewclusTrkPt7.TrackMinPt = cms.double(7.0)
+process.p += process.L1TkElectronsNewclusTrkPt7
+
+process.L1TkPhotonsNewclus = process.L1TkPhotons.clone()
+process.L1TkPhotonsNewclus.L1EGammaInputTag = cms.InputTag("SLHCL1ExtraParticlesNewClustering","EGamma")
+process.p += process.L1TkPhotonsNewclus
+
 process.L1TkIsoElectronsNewclus = process.L1TkIsoElectrons.clone()
 process.L1TkIsoElectronsNewclus.L1EGammaInputTag = cms.InputTag("SLHCL1ExtraParticlesNewClustering","EGamma")
 process.p += process.L1TkIsoElectronsNewclus
+
 
 #L1TkMuons
  #--- Now run the L1TkMuonParticleProducer
