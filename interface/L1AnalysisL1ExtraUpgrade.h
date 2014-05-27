68edbf2c41a6b81a30b843aef70ff71eab939cd5
Georgia <georgia.karapostoli@gmail.com>
2014-05-27 18:19:14 +0200

    change TkPrimaryVertex header name

--- interface/L1AnalysisL1ExtraUpgrade.h
+++ interface/L1AnalysisL1ExtraUpgrade.h
@@ -22,7 +22,7 @@
 
 #include "DataFormats/L1TrackTrigger/interface/L1TkMuonParticle.h"
 #include "DataFormats/L1TrackTrigger/interface/L1TkMuonParticleFwd.h"
-#include "DataFormats/L1TrackTrigger/interface/L1TrackPrimaryVertex.h"
+#include "DataFormats/L1TrackTrigger/interface/L1TkPrimaryVertex.h"
 #include "DataFormats/L1TrackTrigger/interface/L1TkEtMissParticle.h"
 #include "DataFormats/L1TrackTrigger/interface/L1TkEtMissParticleFwd.h"
 #include "DataFormats/L1TrackTrigger/interface/L1TkEmParticle.h"
